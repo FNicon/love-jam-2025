e4dbb5d96082ab93ebf4db6a8bf69a9f494b0a3b
Finiko Kasula Novenda <mybeddrom@gmail.com>
2025-03-21 09:33:25 +0900

    Added temporary implementation of different color for player and enemy

--- game/src/gameplay/vote/votetype.lua
+++ game/src/gameplay/vote/votetype.lua
@@ -4,16 +4,19 @@ local votetypes = {
   support = {
     label = "support",
     color = palette['green'][3],
+    highlight = palette['green'][2],
     progress = true,
   },
   oppose = {
    label = "oppose",
    color = palette['orange'][3],
+   highlight = palette['orange'][2],
    progress = true,
   },
   tie = {
     label = "tie",
     color = palette['blue'][3],
+    highlight = palette['blue'][2],
     progress = false,
   }
 }
